9d7d1171511eb2a62ad84b55722b0c4f8327df12
Admin <Admin@ADMIN-PC>
2024-12-05 17:51:48 +0700

    Moi1

---
+++ client/src/pages/not-found/index.jsx
@@ -0,0 +1,4 @@
+function NotFound() {
+    return <div> Page doesn't exists </div>;
+}
+export default NotFound ;
---
+++ server/controllers/auth/auth-controller.js
@@ -0,0 +1,54 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
+
+
+//REGISTER
+const registerUser = async (req, res) => {
+    const { userName, email, password } = req.body;
+
+    try {
+        
+        //const hashPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({
+            userName,
+            email,
+            password, //: hashPassword,
+        })
+
+        await newUser.save()
+        res.status(200).json({
+            success: true,
+            message: "Registration successful",
+        });
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+        success: false,
+        message: "Some error occured",
+    });
+    }
+};
+
+
+//LOGIN
+const login = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(e);
+        res.status(500).json({
+        success: false,
+        message: "Some error occured",
+    });
+    }
+};
+
+//lOGOUT
+
+
+//Auth Middleware
+
+
+module.exports = {registerUser};
