85083eb51848497b80c0482140ab1ee8aeaa80b5
Admin <Admin@ADMIN-PC>
2024-12-07 03:54:22 +0700

    Moi8

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -4,11 +4,13 @@ import { Input } from "../ui/input";
 import { FileIcon, UploadCloudIcon, XIcon } from "lucide-react";
 import { Button } from "../ui/button";
 import axios from "axios";
+import { Skeleton } from "../ui/skeleton";
 
 
 function ProductImageUpload({
     imageFile,
     setImageFile,
+    imageLoadingState,
     uploadedImageUrl,
     setUploadImageUrl,
     setImageLoadingState,
@@ -83,6 +85,8 @@ function ProductImageUpload({
                         <span>Drag & drop or click to upload image</span>
                     </Label> 
                     ) : (
+                        imageLoadingState ?
+                        <Skeleton className="h-10 bg-gray-100"/> :
                     <div className="flex items-center justify-between">
                         <div className="flex items-center">
                             <FileIcon className="w-8 text-primary mr-2 h-8"/>
--- client/src/pages/admin-view/product.jsx
+++ client/src/pages/admin-view/product.jsx
@@ -3,7 +3,17 @@ import CommonForm from "@/components/common/form";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle  } from "@/components/ui/sheet";
 import { addProductFormElements } from "@/config";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addNewProduct,
+  deleteProduct,
+  editProduct,
+  fetchAllProducts,
+} from "@/store/admin/products-slice";
+import { data } from "react-router-dom";
+import { useToast } from "@/hooks/use-toast";
+
 
 const initialFormData = {
   image: null,
@@ -19,22 +29,45 @@ const initialFormData = {
 
 function AdminProducts() {
 
-    const [openCreateProductDialog, setOpenCreateProductDialog] = 
+    const [openCreateProductDialog, setOpenCreateProductsDialog] = 
       useState (false);
       const [formData,setFormData] = useState(initialFormData);
       const [imageFile,setImageFile] = useState(null);
       const [uploadedImageUrl,setUploadImageUrl] = useState("");
       const [imageLoadingState, setImageLoadingState] = useState(false);
+      const { productList } = useSelector((state) => state.adminProducts);
+      const dispatch = useDispatch();
+      const {toast} = useToast ();
 
-      function onSubmit(){}
+      function onSubmit(event){
+        event.preventDefault();
+        dispatch(addNewProduct({
+          ...formData,
+          image: uploadedImageUrl,
+        })).then((data)=> {
+          if(data?.payload?.success){
+            dispatch(fetchAllProducts())
+            setOpenCreateProductsDialog(false);
+            setImageFile(null);
+            setFormData(initialFormData);
+            toast({
+              title: "Product add successfully",
+            });
+          }
+        })
+      }
 
-        console.log(formData,"formData");
+      useEffect(()=>{
+        dispatch(fetchAllProducts());
+      },[dispatch])
+
+        console.log(productList, uploadedImageUrl, "productList");
 
       
 
     return <Fragment>
       <div className="mb-5 w-full flex justify-end">
-        <Button onClick={()=>setOpenCreateProductDialog(true)}>Add New Product</Button>
+        <Button onClick={()=>setOpenCreateProductsDialog(true)}>Add New Product</Button>
       </div>
       <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
       <Sheet 
@@ -55,6 +88,7 @@ function AdminProducts() {
           uploadedImageUrl={uploadedImageUrl} 
           setUploadImageUrl={setUploadImageUrl} 
           setImageLoadingState ={setImageLoadingState}
+          imageLoadingState ={imageLoadingState}
           />
           <div className="py-6">
             <CommonForm onSubmit={onSubmit} formData={formData} setFormData={setFormData} buttonText='Add'
