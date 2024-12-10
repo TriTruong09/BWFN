87a4184216e9ab9838d3e622bc6fcd188189e027
Admin <Admin@ADMIN-PC>
2024-12-11 06:15:03 +0700

    Moi18-Checkout1

--- client/src/pages/shopping-view/checkout.jsx
+++ client/src/pages/shopping-view/checkout.jsx
@@ -1,18 +1,62 @@
 import img from "../../assets/banner-6.jpg";
-
 import Address from "@/components/shopping-view/address";
+import { useDispatch, useSelector } from "react-redux";
+import UserCartItemsContent from "@/components/shopping-view/cart-items-content";
+import { Button } from "@/components/ui/button";
 
 function ShoppingCheckout() {
-    return ( 
-    <div className="flex flex-col">
+    const { cartItems } = useSelector((state) => state.shopCart);
+
+    console.log(cartItems);
+
+    const totalCartAmount =
+        cartItems && cartItems.items && cartItems.items.length > 0
+            ? cartItems.items.reduce(
+                (sum, currentItem) =>
+                    sum +
+                    (currentItem?.salePrice > 0
+                        ? currentItem?.salePrice
+                        : currentItem?.price) *
+                    currentItem?.quantity,
+                0
+            )
+            : 0;
+
+    return (
+        <div
+            className="flex flex-col">
             <div className="relative h-[300px] w-full overflow-hidden">
-                //<img src={img} className="h-full w-full object-cover object-center" />
+                {<img src={img} className="h-full w-full object-cover object-center" /> }
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 mt-5 p-5">
                 <Address />
+                <div className="flex flex-col gap-4">
+                    {cartItems && cartItems.items && cartItems.items.length > 0
+                        ? cartItems.items.map((item) => (
+                            <UserCartItemsContent cartItem={item} />
+                        ))
+                        : null}
+                    <div className="mt-8 space-y-4">
+                        <div className="flex justify-between">
+                            <span className="font-bold">Tổng tiền</span>
+                            {/* Hiển thị tổng tiền theo VNĐ */}
+                            <span className="font-bold">
+                                {new Intl.NumberFormat("vi-VN", {
+                                    style: "currency",
+                                    currency: "VND",
+                                }).format(totalCartAmount)}
+                            </span>
+                        </div>
+                    </div>
+                    <div className="mt-4 w-full">
+                        <Button className="w-full">Thanh toán bằng Paypal</Button>
+                    </div>
+                </div>
+                <div>
+                </div>
             </div>
-    </div> 
-     );
+        </div>
+    );
 }
 
 export default ShoppingCheckout;
